d4f4ae287dd466bf5bce3f702f3799c3ebe4b1ec
Darya <daryalaptsevich@gmail.com>
2017-04-03 13:46:07 +0300

    Добавлена загрузка и кнопки для загрузки мультимедиа

--- video-editor/filemanagerwidget.cpp
+++ video-editor/filemanagerwidget.cpp
@@ -1,14 +1,83 @@
 #include "filemanagerwidget.h"
 #include "ui_filemanagerwidget.h"
 
+#include <QFileDialog>
+#include <QStandardItem>
+
+#include "QtDebug"
+
 FileManagerWidget::FileManagerWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::FileManagerWidget)
 {
     ui->setupUi(this);
+    homeDir = "/";
+
+    ui->addAudio->setIcon(QIcon("../addAudio.png"));
+    ui->addVideo->setIcon(QIcon("../addVideo.png"));
+    ui->addImage->setIcon(QIcon("../addImage.png"));
 }
 
 FileManagerWidget::~FileManagerWidget()
 {
     delete ui;
 }
+
+void FileManagerWidget::on_addAudio_clicked()
+{
+    QString fileName;
+    QStringList files = QFileDialog::getOpenFileNames(
+                            this,
+                            "Выберите аудио файлы",
+                            homeDir,
+                            "Аудиофайлы (*.mp3 *.m4a *.wav *.aif *.aifc *.aiff *.snd *.au *.mpa *.mp2 *.wma *.asf)");
+
+    foreach (QString filePath, files) {
+        fileName = QDir(filePath).dirName();
+        audioFiles.append(new QStandardItem(QDir(filePath).dirName()));
+    }
+    if (files.size() > 0){
+        homeDir = files.at(0);
+        homeDir.left(homeDir.lastIndexOf("/")+1);
+    }
+}
+
+void FileManagerWidget::on_addVideo_clicked()
+{
+    QString fileName;
+    QStringList files = QFileDialog::getOpenFileNames(
+                            this,
+                            "Выберите видео файлы",
+                            homeDir,
+                            "Видео (*.avi *.mpg *.m1v *.mp2 *.mp2v *.mpeg *.mpe *.mpv2 *.wm *wmv *.asf)");
+
+    foreach (QString filePath, files) {
+        fileName = QDir(filePath).dirName();
+        videoFiles.append(new QStandardItem(QDir(filePath).dirName()));
+    }
+
+    if (files.size() > 0){
+        homeDir = files.at(0);
+        homeDir.left(homeDir.lastIndexOf("/")+1);
+    }
+}
+
+void FileManagerWidget::on_addImage_clicked()
+{
+    QString fileName;
+    QStringList files = QFileDialog::getOpenFileNames(
+                            this,
+                            "Выберите изображения",
+                            homeDir,
+                            "Изображения (*.jpg *.jpeg *.jpe *.jfif *.gif *.png *.bmp *.dib *.tif *.tiff *.wmf *.emf)");
+
+    foreach (QString filePath, files) {
+        fileName = QDir(filePath).dirName();
+        imageFiles.append(new QStandardItem(QDir(filePath).dirName()));
+    }
+    if (files.size() >0){
+        homeDir = files.at(0);
+        homeDir.left(homeDir.lastIndexOf("/")+1);
+    }
+}
+
--- video-editor/filemanagerwidget.h
+++ video-editor/filemanagerwidget.h
@@ -2,6 +2,7 @@
 #define FILEMANAGERWIDGET_H
 
 #include <QWidget>
+#include <QStandardItem>
 
 namespace Ui {
 class FileManagerWidget;
@@ -15,8 +16,21 @@ public:
     explicit FileManagerWidget(QWidget *parent = 0);
     ~FileManagerWidget();
 
+private slots:
+
+    void on_addAudio_clicked();
+
+    void on_addVideo_clicked();
+
+    void on_addImage_clicked();
+
 private:
     Ui::FileManagerWidget *ui;
+    QList <QStandardItem *> audioFiles;
+    QList <QStandardItem *> videoFiles;
+    QList <QStandardItem *> imageFiles;
+    QString homeDir;
+
 };
 
 #endif // FILEMANAGERWIDGET_H
