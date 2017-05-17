ae5b86669fab496764eac8f80791c0cf2e793571
darya <dashka_lala@mail.ru>
2017-05-17 23:12:41 +0300

    мерж

--- video-editor/editorwidget.cpp
+++ video-editor/editorwidget.cpp
@@ -21,3 +21,43 @@ void EditorWidget::changeSize()
     int h = 130;
     ui->verticalLayoutWidget->setGeometry(70,10,w,h);
 }
+
+void EditorWidget::addToPlayList(PlayItem item)
+{
+
+    item.absBegin = length + 1;
+    item.absEnd = item.absBegin + item.end - item.begin;
+    list.append(item);
+    length += (item.end - item.begin);
+
+    QWidget* itemWidget = new QWidget();
+    itemWidget->setAutoFillBackground(true);
+    QPalette pal(palette());
+    pal.setColor(QPalette::Background, Qt::gray);
+    itemWidget->setPalette(pal);
+    ui->tracklist->setAlignment(Qt::AlignLeft);
+    ui->audioTrack->setAlignment(Qt::AlignLeft);
+    itemWidget->setMinimumWidth((item.end - item.begin)/100);
+    itemWidget->setMaximumWidth((item.end - item.begin)/100);
+
+    QLabel* name = new QLabel(itemWidget);
+    QString str = item.url.right(item.url.length()-item.url.lastIndexOf('/')-1);
+    name->setText(str);
+    QString extension = item.url.right(3);
+    if(extension == "mp3"){
+        ui->audioTrackWidget->setVisible(false);
+        ui->audioTrack->addWidget(itemWidget);
+    }
+
+    if(extension == "avi"){
+        ui->widget->setVisible(false);
+        ui->tracklist->addWidget(itemWidget);
+    }
+
+}
+
+
+void EditorWidget::on_pushButton_clicked()
+{
+    emit playUsersList(list,length);
+}
